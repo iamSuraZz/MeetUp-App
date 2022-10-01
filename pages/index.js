288fd89bd3c79d96efa47ff6bb5625258a32369d
SuraZz007 <surajdev9886@gmail.com>
2022-10-01 17:13:56 +0530

    output list of meetups and add new meetup form

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup!",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a second meetup!",
+  },
+];
+
+const HomePage = () => {
+  return (
+    <div>
+      <MeetupList meetups={DUMMY_MEETUPS} />
+    </div>
+  );
+};
+export default HomePage;
